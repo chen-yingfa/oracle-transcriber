0bd2336eb47f65e684f4fb981ec297e4db0acae0
Donny Chan <chenyingfa1999@qq.com>
2023-07-04 02:00:23 +0800

    Update

--- transcription/models/base_model.py
+++ transcription/models/base_model.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import torch
 from collections import OrderedDict
 from abc import ABC, abstractmethod
@@ -6,35 +7,58 @@ from . import networks
 
 
 class BaseModel(ABC):
-    """This class is an abstract base class (ABC) for models.
+    """
+    This class is an abstract base class (ABC) for models.
     To create a subclass, you need to implement the following five functions:
-        -- <__init__>:                      initialize the class; first call BaseModel.__init__(self, opt).
-        -- <set_input>:                     unpack data from dataset and apply preprocessing.
+        -- <__init__>:                      initialize the class; first call
+            BaseModel.__init__(self, opt).
+        -- <set_input>:                     unpack data from dataset and apply
+            preprocessing.
         -- <forward>:                       produce intermediate results.
-        -- <optimize_parameters>:           calculate losses, gradients, and update network weights.
-        -- <modify_commandline_options>:    (optionally) add model-specific options and set default options.
+        -- <optimize_parameters>:           calculate losses, gradients, and
+            update network weights.
+        -- <modify_commandline_options>:    (optionally) add model-specific
+            options and set default options.
     """
 
     def __init__(self, opt):
-        """Initialize the BaseModel class.
+        """
+        Initialize the BaseModel class.
 
         Parameters:
-            opt (Option class)-- stores all the experiment flags; needs to be a subclass of BaseOptions
+            opt (Option class)-- stores all the experiment flags; needs to be a
+                subclass of BaseOptions
 
         When creating your custom class, you need to implement your own initialization.
         In this function, you should first call <BaseModel.__init__(self, opt)>
         Then, you need to define four lists:
-            -- self.loss_names (str list):          specify the training losses that you want to plot and save.
-            -- self.model_names (str list):         define networks used in our training.
-            -- self.visual_names (str list):        specify the images that you want to display and save.
-            -- self.optimizers (optimizer list):    define and initialize optimizers. You can define one optimizer for each network. If two networks are updated at the same time, you can use itertools.chain to group them. See cycle_gan_model.py for an example.
+            -- self.loss_names (str list):          specify the training losses
+                that you want to plot and save.
+            -- self.model_names (str list):         define networks used in our
+                training.
+            -- self.visual_names (str list):        specify the images that you
+                want to display and save.
+            -- self.optimizers (optimizer list):    define and initialize
+                optimizers. You can define one optimizer for each network.
+                If two networks are updated at the same time, you can use
+                itertools.chain to group them. See cycle_gan_model.py for an example.
         """
         self.opt = opt
         self.gpu_ids = opt.gpu_ids
         self.isTrain = opt.isTrain
-        self.device = torch.device('cuda:{}'.format(self.gpu_ids[0])) if self.gpu_ids else torch.device('cpu')  # get device name: CPU or GPU
-        self.save_dir = os.path.join(opt.checkpoints_dir, opt.name)  # save all the checkpoints to save_dir
-        if opt.preprocess != 'scale_width':  # with [scale_width], input images might have different sizes, which hurts the performance of cudnn.benchmark.
+        self.device = (
+            torch.device("cuda:{}".format(self.gpu_ids[0]))
+            if self.gpu_ids
+            else torch.device("cpu")
+        )  # get device name: CPU or GPU
+        self.save_dir = Path(
+            opt.checkpoints_dir, opt.name
+        )  # save all the checkpoints to save_dir
+        if (
+            opt.preprocess != "scale_width"
+        ):
+            # with [scale_width], input images might have different sizes,
+            # which hurts the performance of cudnn.benchmark.
             torch.backends.cudnn.benchmark = True
         self.loss_names = []
         self.model_names = []
@@ -45,11 +69,13 @@ class BaseModel(ABC):
 
     @staticmethod
     def modify_commandline_options(parser, is_train):
-        """Add new model-specific options, and rewrite default values for existing options.
+        """
+        Add new model-specific options, and rewrite default values for existing options.
 
         Parameters:
             parser          -- original option parser
-            is_train (bool) -- whether training phase or test phase. You can use this flag to add training-specific or test-specific options.
+            is_train (bool) -- whether training phase or test phase. You can use
+                this flag to add training-specific or test-specific options.
 
         Returns:
             the modified parser.
@@ -58,7 +84,9 @@ class BaseModel(ABC):
 
     @abstractmethod
     def set_input(self, input):
-        """Unpack input data from the dataloader and perform necessary pre-processing steps.
+        """
+        Unpack input data from the dataloader and perform necessary pre-processing
+        steps.
 
         Parameters:
             input (dict): includes the data itself and its metadata information.
@@ -67,24 +95,35 @@ class BaseModel(ABC):
 
     @abstractmethod
     def forward(self):
-        """Run forward pass; called by both functions <optimize_parameters> and <test>."""
+        """
+        Run forward pass; called by both functions <optimize_parameters> and
+        <test>.
+        """
         pass
 
     @abstractmethod
     def optimize_parameters(self):
-        """Calculate losses, gradients, and update network weights; called in every training iteration"""
+        """
+        Calculate losses, gradients, and update network weights; called in
+        every training iteration
+        """
         pass
 
     def setup(self, opt):
-        """Load and print networks; create schedulers
+        """
+        Load and print networks; create schedulers
 
         Parameters:
-            opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
+            opt (Option class) -- stores all the experiment flags; needs to be
+            a subclass of BaseOptions
         """
         if self.isTrain:
-            self.schedulers = [networks.get_scheduler(optimizer, opt) for optimizer in self.optimizers]
+            self.schedulers = [
+                networks.get_scheduler(optimizer, opt) for optimizer in self.optimizers
+            ]
         if not self.isTrain or opt.continue_train:
-            load_suffix = 'iter_%d' % opt.load_iter if opt.load_iter > 0 else opt.epoch
+            load_suffix = "iter_%d" % opt.load_iter if opt.load_iter > 0 else opt.epoch
+            print("Loading:", load_suffix)
             self.load_networks(load_suffix)
         self.print_networks(opt.verbose)
 
@@ -92,13 +131,14 @@ class BaseModel(ABC):
         """Make models eval mode during test time"""
         for name in self.model_names:
             if isinstance(name, str):
-                net = getattr(self, 'net' + name)
+                net = getattr(self, "net" + name)
                 net.eval()
 
     def test(self):
         """Forward function used in test time.
 
-        This function wraps <forward> function in no_grad() so we don't save intermediate steps for backprop
+        This function wraps <forward> function in no_grad() so we don't save
+            intermediate steps for backprop
         It also calls <compute_visuals> to produce additional visualization results
         """
         with torch.no_grad():
@@ -110,23 +150,28 @@ class BaseModel(ABC):
         pass
 
     def get_image_paths(self):
-        """ Return image paths that are used to load current data"""
+        """Return image paths that are used to load current data"""
         return self.image_paths
 
     def update_learning_rate(self):
-        """Update learning rates for all the networks; called at the end of every epoch"""
-        old_lr = self.optimizers[0].param_groups[0]['lr']
+        """
+        Update learning rates for all the networks; called at the end of every epoch
+        """
+        old_lr = self.optimizers[0].param_groups[0]["lr"]
         for scheduler in self.schedulers:
-            if self.opt.lr_policy == 'plateau':
+            if self.opt.lr_policy == "plateau":
                 scheduler.step(self.metric)
             else:
                 scheduler.step()
 
-        lr = self.optimizers[0].param_groups[0]['lr']
-        print('learning rate %.7f -> %.7f' % (old_lr, lr), flush=True)
+        lr = self.optimizers[0].param_groups[0]["lr"]
+        print("learning rate %.7f -> %.7f" % (old_lr, lr), flush=True)
 
     def get_current_visuals(self):
-        """Return visualization images. train.py will display these images with visdom, and save the images to a HTML"""
+        """
+        Return visualization images. train.py will display these images with
+        visdom, and save the images to a HTML
+        """
         visual_ret = OrderedDict()
         for name in self.visual_names:
             if isinstance(name, str):
@@ -134,24 +179,31 @@ class BaseModel(ABC):
         return visual_ret
 
     def get_current_losses(self):
-        """Return traning losses / errors. train.py will print out these errors on console, and save them to a file"""
+        """
+        Return traning losses / errors. train.py will print out these errors
+        on console, and save them to a file
+        """
         errors_ret = OrderedDict()
         for name in self.loss_names:
             if isinstance(name, str):
-                errors_ret[name] = float(getattr(self, 'loss_' + name))  # float(...) works for both scalar tensor and float number
+                errors_ret[name] = float(
+                    getattr(self, "loss_" + name)
+                )  # float(...) works for both scalar tensor and float number
         return errors_ret
 
     def save_networks(self, epoch):
-        """Save all the networks to the disk.
+        """
+        Save all the networks to the disk.
 
         Parameters:
-            epoch (int) -- current epoch; used in the file name '%s_net_%s.pth' % (epoch, name)
+            epoch (int) -- current epoch; used in the file name
+                '%s_net_%s.pth' % (epoch, name)
         """
         for name in self.model_names:
             if isinstance(name, str):
-                save_filename = '%s_net_%s.pth' % (epoch, name)
+                save_filename = "%s_net_%s.pth" % (epoch, name)
                 save_path = os.path.join(self.save_dir, save_filename)
-                net = getattr(self, 'net' + name)
+                net = getattr(self, "net" + name)
 
                 if len(self.gpu_ids) > 0 and torch.cuda.is_available():
                     torch.save(net.module.cpu().state_dict(), save_path)
@@ -163,58 +215,68 @@ class BaseModel(ABC):
         """Fix InstanceNorm checkpoints incompatibility (prior to 0.4)"""
         key = keys[i]
         if i + 1 == len(keys):  # at the end, pointing to a parameter/buffer
-            if module.__class__.__name__.startswith('InstanceNorm') and \
-                    (key == 'running_mean' or key == 'running_var'):
+            if module.__class__.__name__.startswith("InstanceNorm") and (
+                key == "running_mean" or key == "running_var"
+            ):
                 if getattr(module, key) is None:
-                    state_dict.pop('.'.join(keys))
-            if module.__class__.__name__.startswith('InstanceNorm') and \
-               (key == 'num_batches_tracked'):
-                state_dict.pop('.'.join(keys))
+                    state_dict.pop(".".join(keys))
+            if module.__class__.__name__.startswith("InstanceNorm") and (
+                key == "num_batches_tracked"
+            ):
+                state_dict.pop(".".join(keys))
         else:
-            self.__patch_instance_norm_state_dict(state_dict, getattr(module, key), keys, i + 1)
+            self.__patch_instance_norm_state_dict(
+                state_dict, getattr(module, key), keys, i + 1
+            )
 
     def load_networks(self, epoch):
         """Load all the networks from the disk.
 
         Parameters:
-            epoch (int) -- current epoch; used in the file name '%s_net_%s.pth' % (epoch, name)
+            epoch (int) -- current epoch;
+                used in the file name '%s_net_%s.pth' % (epoch, name)
         """
         for name in self.model_names:
-            if isinstance(name, str):
-                load_filename = '%s_net_%s.pth' % (epoch, name)
-                load_path = os.path.join(self.save_dir, load_filename)
-                net = getattr(self, 'net' + name)
-                if isinstance(net, torch.nn.DataParallel):
-                    net = net.module
-                print('loading the model from %s' % load_path)
-                # if you are using PyTorch newer than 0.4 (e.g., built from
-                # GitHub source), you can remove str() on self.device
-                state_dict = torch.load(load_path, map_location=str(self.device))
-                if hasattr(state_dict, '_metadata'):
-                    del state_dict._metadata
+            assert isinstance(name, str)
+            load_filename = f"{epoch}_net_{name}.pth"
+            load_path = Path(self.save_dir, load_filename)
+            net = getattr(self, "net" + name)
+            if isinstance(net, torch.nn.DataParallel):
+                net = net.module
+            print(f"loading the model from {load_path}")
+            state_dict = torch.load(load_path, map_location=self.device)
+            if hasattr(state_dict, "_metadata"):
+                del state_dict._metadata
 
-                # patch InstanceNorm checkpoints prior to 0.4
-                for key in list(state_dict.keys()):  # need to copy keys here because we mutate in loop
-                    self.__patch_instance_norm_state_dict(state_dict, net, key.split('.'))
-                net.load_state_dict(state_dict)
+            # patch InstanceNorm checkpoints prior to 0.4
+            for key in list(
+                state_dict.keys()
+            ):  # need to copy keys here because we mutate in loop
+                self.__patch_instance_norm_state_dict(state_dict, net, key.split("."))
+            net.load_state_dict(state_dict)
 
     def print_networks(self, verbose):
-        """Print the total number of parameters in the network and (if verbose) network architecture
+        """
+        Print the total number of parameters in the network and (if verbose)
+        network architecture
 
         Parameters:
             verbose (bool) -- if verbose: print the network architecture
         """
-        print('---------- Networks initialized -------------')
+        print("---------- Networks initialized -------------")
         for name in self.model_names:
             if isinstance(name, str):
-                net = getattr(self, 'net' + name)
+                net = getattr(self, "net" + name)
                 num_params = 0
                 for param in net.parameters():
                     num_params += param.numel()
                 if verbose:
                     print(net)
-                print('[Network %s] Total number of parameters : %.3f M' % (name, num_params / 1e6))
-        print('-----------------------------------------------')
+                print(
+                    "[Network %s] Total number of parameters : %.3f M"
+                    % (name, num_params / 1e6)
+                )
+        print("-----------------------------------------------")
 
     def set_requires_grad(self, nets, requires_grad=False):
         """Set requies_grad=Fasle for all the networks to avoid unnecessary computations
--- transcription/transcribe.py
+++ transcription/transcribe.py
@@ -1,5 +1,6 @@
-from PIL import Image
+from PIL import Image, ImageFile
 import torch
+from pathlib import Path
 
 from options.test_options import TestOptions
 from models import create_model
@@ -7,32 +8,35 @@ from data.base_dataset import get_transform, get_params
 import util
 
 
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+
 def get_opt():
     opt = TestOptions().parse(output_opt=False)
     opt.batch_size = 64
-    opt.checkpoints_dir = './checkpoints'
+    opt.checkpoints_dir = "./checkpoints"
     opt.crop_size = 128
-    opt.dataset_mode = 'aligned'
+    opt.dataset_mode = "aligned"
     # opt.dataroot = './datasets/oracle2transcription'
-    opt.dataroot = ''
-    opt.direction = 'AtoB'
+    opt.dataroot = ""
+    opt.direction = "AtoB"
     opt.display_id = -1
     opt.display_winsize = 128
     opt.input_nc = 1
     opt.load_size = 128
-    opt.model = 'pix2pix'
-    opt.name = 'transcriber'
-    opt.norm = 'batch'
-    opt.netG = 'unet_128'
+    opt.model = "pix2pix"
+    opt.name = "rt7381_aligned_L100_replace0.75_hmask0.25_smask0.25"
+    opt.norm = "batch"
+    opt.netG = "unet_128"
     opt.no_flip = True
     opt.num_threads = 0
     opt.output_nc = 1
     opt.serial_batches = True
 
     opt.num_threads = 0
-    opt.batch_size = 1 
+    opt.batch_size = 1
     opt.serial_batches = True
-    opt.no_flip = True 
+    opt.no_flip = True
     opt.display_id = -1
     return opt
 
@@ -40,16 +44,18 @@ def get_opt():
 opt = get_opt()
 
 
-def preprocess_img(img: Image) -> Image:
-    def pad_and_resize(img: Image, shape: tuple=(96, 96), pad: int=255) -> Image:
+def preprocess_img(img: Image.Image) -> Image.Image:
+    def pad_and_resize(
+        img: Image, shape: tuple = (96, 96), pad: int = 255
+    ) -> Image.Image:
         w, h = img.size
         longest = max(w, h)
         paste_pos = ((longest - w) // 2, (longest - h) // 2)
-        new_img = Image.new('L', (longest, longest), color=pad)
+        new_img = Image.new("L", (longest, longest), color=pad)
         new_img.paste(img, paste_pos)
         new_img = new_img.resize(shape)
         return new_img
-        
+
     img = pad_and_resize(img)
     transform_params = get_params(opt, img.size)
     transform = get_transform(opt, transform_params, grayscale=(opt.input_nc == 1))
@@ -67,30 +73,35 @@ def transcribe(img: Image, model) -> Image:
 
 
 def get_model():
-    # print('Creating model')
+    print("Creating model")
     model = create_model(opt)
-    # print('Setting up model')
+    print("Setting up model")
     model.setup(opt)
     model.eval()
     return model
 
 
 def main():
-    print('Getting model')
+    print("Getting model")
     model = get_model()
 
-    print('Transcribing')
+    print("Transcribing")
     # path = './datasets/oracle2transcription/test/H00072反_告.png'
     # img = Image.open(path)
     # w, h = img.size
     # img = img.crop((0, 0, w//2, h))
     # img.save('orig.png')
-    
-    img = Image.open('orig.png')
-    img = transcribe(img, model)
-    print('Saving to transcript.png')
-    img.save('transcript.png')
+    src_dir = Path("../../data/H32384_detection_result")
+    dst_dir = Path("../../data/H32384_detection_result_transcribed")
+    dst_dir.mkdir(exist_ok=True, parents=True)
+    img_files = sorted(src_dir.glob("*.jpg"))
+    print(img_files)
+    for img_file in img_files:
+        img = Image.open(img_file)
+        print("Transcribing:", img_file.name)
+        img = transcribe(img, model)
+        img.save(dst_dir / img_file.name)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
